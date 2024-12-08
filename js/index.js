52e9c8f4cb3b32ca866c585e6b06fd9288702a7a
Faiz Alam <fz007660@gmail.com>
2024-12-08 12:23:20 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const faqBx =   document.querySelectorAll('.faq-bx');
+
+for(let i = 0; i<= faqBx.length; i++){
+    faqBx[i].addEventListener("click", () => {
+        for(let j = 0;  j < faqBx.length; j++){
+            faqBx[j].classList.remove('show')
+        }
+        faqBx[i].classList.add('show')
+    })
+}
+
+
