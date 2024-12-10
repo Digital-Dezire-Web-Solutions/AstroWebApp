9ec2e9ad5c27203f6ad9dcef7150d14766b2d61b
Faiz Alam <fz007660@gmail.com>
2024-12-10 14:51:55 +0530

    Kundaliform

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+
+const newAcntBtn =  document.querySelector('#newAcnt');
+const formBx =  document.querySelector('.login-bx');
+const haveAnAcnt =  document.querySelector('#signInBtn');
+const formComp = document.querySelector('.login-reg-comp');
+const closeForm = document.querySelector('.close-form-btn');
+const loginBtn =  document.querySelector('#login_btn');
+
+newAcntBtn.addEventListener("click", () => {
+    formBx.classList.add('registeractive');
+})
+
+
+haveAnAcnt.addEventListener("click", () => {
+    formBx.classList.remove('registeractive');
+    formBx.scrollTop = 0
+})
+
+formBx.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+
+formComp.addEventListener("click", () => {
+    formComp.classList.remove('formactive');
+})
+
+closeForm.addEventListener("click", () => {
+    formComp.classList.remove('formactive');
+})
+
+loginBtn.addEventListener("click", () => {
+    formComp.classList.add('formactive')
+})
+
 const faqBx =   document.querySelectorAll('.faq-bx');
 
 for(let i = 0; i<= faqBx.length; i++){
