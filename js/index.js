8a9cacbd9b885ac7eb6704fcd295919d800c3fbd
Faiz Alam <fz007660@gmail.com>
2024-12-14 11:13:58 +0530

    headerUpdated

--- js/index.js
+++ js/index.js
@@ -32,15 +32,6 @@ loginBtn.addEventListener("click", () => {
     formComp.classList.add('formactive')
 })
 
-const faqBx =   document.querySelectorAll('.faq-bx');
 
-for(let i = 0; i<= faqBx.length; i++){
-    faqBx[i].addEventListener("click", () => {
-        for(let j = 0;  j < faqBx.length; j++){
-            faqBx[j].classList.remove('show')
-        }
-        faqBx[i].classList.add('show')
-    })
-}
 
 
